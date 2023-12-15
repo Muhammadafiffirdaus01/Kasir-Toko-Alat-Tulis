6e1a6f04e3d6381f7e931408b89f0be4b4f12a37
Muhammadafiffirdaus01 <111755206+Muhammadafiffirdaus01@users.noreply.github.com>
2023-12-15 21:15:23 +0700

    Add files via upload

---
+++ database penjualan.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2023 at 09:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('darujinan', '2023');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `ID_Barang` varchar(10) NOT NULL,
+  `Nama_Barang` varchar(50) DEFAULT NULL,
+  `Jenis` varchar(20) DEFAULT NULL,
+  `HargaBeli` decimal(10,0) DEFAULT NULL,
+  `HargaJual` decimal(10,0) DEFAULT NULL,
+  `Stok` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`ID_Barang`, `Nama_Barang`, `Jenis`, `HargaBeli`, `HargaJual`, `Stok`) VALUES
+('BR001', 'Faber', 'Pensil', 2000, 3000, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `ID_Member` varchar(10) NOT NULL,
+  `Nama_Member` text NOT NULL,
+  `No_Telepon` text NOT NULL,
+  `Tanggal_Aktif` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`ID_Member`, `Nama_Member`, `No_Telepon`, `Tanggal_Aktif`) VALUES
+('MB001', 'Daru', '08953', '2023');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `NoFaktur` varchar(20) NOT NULL,
+  `Tanggal` varchar(20) DEFAULT NULL,
+  `ID_Member2` varchar(10) DEFAULT NULL,
+  `TotalBeli` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualanrinci`
+--
+
+CREATE TABLE `penjualanrinci` (
+  `NoFaktur` varchar(20) DEFAULT NULL,
+  `ID_Barang` varchar(10) DEFAULT NULL,
+  `Nama_Barang` varchar(50) DEFAULT NULL,
+  `Jumlah` int(11) DEFAULT NULL,
+  `Harga` decimal(10,0) DEFAULT NULL,
+  `Total` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penjualanrinci`
+--
+
+INSERT INTO `penjualanrinci` (`NoFaktur`, `ID_Barang`, `Nama_Barang`, `Jumlah`, `Harga`, `Total`) VALUES
+('TR0001', 'P01', 'Kaos Katun', 1, 30000, 30000),
+('TR0002', 'P01', 'Kaos Katun', 40, 30000, 1200000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`ID_Barang`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`NoFaktur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
